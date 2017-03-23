98622cac0f57a30445f3a707cd02279693f32c3b
Margin <jhu_com@163.com>
2017-03-23 10:26:41 +0800

    add tools

---
+++ tools/set_git_proxy.sh
@@ -0,0 +1,9 @@
+#只对github.com
+git config --global http.https://github.com.proxy 'socks5://10.168.88.1:1080'
+
+#取消代理
+#git config --global --unset http.https://github.com.proxy
+
+# https://gist.github.com/laispace/666dd7b27e9116faece6
+
+
---
+++ tools/unset_git_proxy.sh
@@ -0,0 +1,7 @@
+#只对github.com
+#git config --global http.https://github.com.proxy 'socks5://10.168.88.1:1080'
+
+#取消代理
+git config --global --unset http.https://github.com.proxy
+
+
